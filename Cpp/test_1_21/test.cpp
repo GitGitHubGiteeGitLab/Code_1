743408ac1657a0b47237a64072a0f95b2320b880
GitGitHubGiteeGitLab <130532493+GitGitHubGiteeGitLab@users.noreply.github.com>
2024-01-21 16:47:43 +0800

    c++入门

---
+++ Cpp/test_1_21/test.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+
+int Add(int  a, int b)
+{
+    return a + b;
+}
+double Add(double a, double b)
+{
+
+    return a + b;
+}
+
+int func(int a, int b)
+{
+    return a * b;
+}
+
+double func(double a, double b)
+{
+    return a * b;
+}
+
+int main()
+{
+    return 0;
+}
